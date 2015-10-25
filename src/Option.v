2c96a77cd77e91dacd9a2b311255284bccfa8000
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2015-10-25 14:13:45 -0500

    Added an `is_some` predicate along with some properties about it.

--- src/Option.v
+++ src/Option.v
@@ -1,3 +1,5 @@
+Set Implicit Arguments.
+
 Lemma opt_neq_none:
   forall {A:Type} (o:option A),
   o <> None ->
@@ -33,4 +35,84 @@ Proof.
   destruct H.
   apply opt_some_neq in H.
   assumption.
+Qed.
+
+Definition is_some {A:Type} (o:option A) : bool :=
+  match o with
+  | Some _ => true
+  | None => false
+  end.
+
+Lemma is_some_true:
+  forall {A:Type} (o:option A),
+  is_some o = true ->
+  exists v, o = Some v.
+Proof.
+  intros.
+  destruct o.
+  - exists a; trivial.
+  - inversion H.
+Qed.
+
+Lemma some_to_is_some:
+  forall {A:Type} o (x:A),
+  o = Some x ->
+  is_some o = true.
+Proof.
+  intros.
+  subst; simpl; trivial.
+Qed.
+
+Lemma is_some_rw:
+  forall {A:Type} (o:option A),
+  is_some o = true <->
+  exists v, o = Some v.
+Proof.
+  split; intros.
+  - auto using is_some_true.
+  - destruct H; eauto using some_to_is_some.
+Qed.
+
+Lemma is_some_false:
+  forall {A:Type} (o:option A),
+  is_some o = false ->
+  o = None.
+Proof.
+  intros.
+  destruct o.
+  - inversion H.
+  - trivial.
+Qed.
+
+Lemma is_some_none:
+  forall {A:Type} (o:option A),
+  o = None ->
+  is_some o = false.
+Proof.
+  intros.
+  destruct o.
+  - inversion H.
+  - auto.
+Qed.
+
+Lemma is_some_rw_none:
+  forall {A:Type} (o:option A),
+  is_some o = false <-> o = None.
+Proof.
+  split;intros; auto using is_some_false, is_some_none.
+Qed.
+
+Lemma is_some_dec:
+  forall {A:Type} (o:option A),
+  { is_some o = true /\ exists v, o = Some v } + { is_some o = false /\ o = None }.
+Proof.
+  intros.
+  remember (is_some o).
+  symmetry in Heqb.
+  destruct b.
+  - left.
+    apply is_some_true in Heqb.
+    intuition.
+  - right.
+    apply is_some_false in Heqb; intuition.
 Qed.
